085539968ed088777c00e8e4a443fcf7cfc0b41c
mjtechguy <mikejohnsonit@gmail.com>
2022-11-14 19:50:05 +0000

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update
+apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+mkdir -p ~/.fonts
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output ~/.fonts/'MesloLGS NF Regular.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output ~/.fonts/'MesloLGS NF Bold.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output ~/.fonts/'MesloLGS NF Italic.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output ~/.fonts/'MesloLGS NF Bold Italic.ttf'
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" ~/.zshrc
+cp ./.zshrc ~/.zshrc
+cp ./.p10k.zsh ~/.p10k.zsh
+source .zshrc
